attester/attester

commit 93411a13cafb165d123a7ec20eeb1333af453102

Giving access to the PhantomJS sendEvent method from testing tools.

lib/browsers/phantomjs.js
@@ -36,26 +36,54 @@ var logHeader = "[PhantomJS " + id + "]";
 console.log(logHeader + " opening URL " + url);
 
 var page = require('webpage').create();
+
+page.viewportSize = {width: 1016, height: 612};
+
 page.onInitialized = function (arg1) {
     page.evaluate(function () {
         window.attesterConfig = {
             globalErrors: true,
             onDisconnect: function () {
-                console.log("PHANTOMJS-ATTESTER-DISCONNECT");
+                window.callPhantom({
+                    name: 'attesterOnDisconnect'
+                });
+            }
+        };
+        window.phantomJSRobot = {
+            sendEvent: function () {
+                window.callPhantom({
+                    name: 'sendEvent',
+                    args: [].slice.call(arguments,0)
+                });
             }
         };
     });
+    page.evaluate("function(){window.phantomJSRobot.keys=(" + JSON.stringify(page.event.key) + ");}");
 };
 
-page.onConsoleMessage = function (msg) {
-    if (msg == "PHANTOMJS-ATTESTER-DISCONNECT") {
+var callbacks = {
+    'attesterOnDisconnect' : function () {
         if (autoExit) {
             console.log(logHeader + " exiting");
             phantom.exit();
         }
-    } else {
-        console.log(logHeader + " log: ", msg);
+    },
+    'sendEvent' : function (event) {
+        page.sendEvent.apply(page, event.args);
     }
+};
+
+page.onCallback = function (event) {
+    var handler = callbacks[event.name];
+    if (handler) {
+        return handler(event);
+    } else {
+        console.log(logHeader + " the page is trying to call an undefined callback: ",event.name);
+    }
+};
+
+page.onConsoleMessage = function (msg) {
+    console.log(logHeader + " log: ", msg);
 };
 
 page.onError = function (error, stack) {

commit 542f8013fb2db008b4c52bf1dd409311bab9a90e

Fixing an issue introduced by 93411a13cafb165d123a7ec20eeb1333af453102.
Commit 93411a13cafb165d123a7ec20eeb1333af453102 replaced console.log
by window.callPhantom to transmit commands from the web page to
PhantomJS, but there were two places still using console.log.
This commit fixes them.

lib/browsers/phantomjs.js
@@ -37,7 +37,10 @@ console.log(logHeader + " opening URL " + url);
 
 var page = require('webpage').create();
 
-page.viewportSize = {width: 1016, height: 612};
+page.viewportSize = {
+    width: 1016,
+    height: 612
+};
 
 page.onInitialized = function (arg1) {
     page.evaluate(function () {
@@ -53,7 +56,7 @@ page.onInitialized = function (arg1) {
             sendEvent: function () {
                 window.callPhantom({
                     name: 'sendEvent',
-                    args: [].slice.call(arguments,0)
+                    args: [].slice.call(arguments, 0)
                 });
             }
         };
@@ -62,13 +65,13 @@ page.onInitialized = function (arg1) {
 };
 
 var callbacks = {
-    'attesterOnDisconnect' : function () {
+    'attesterOnDisconnect': function () {
         if (autoExit) {
             console.log(logHeader + " exiting");
             phantom.exit();
         }
     },
-    'sendEvent' : function (event) {
+    'sendEvent': function (event) {
         page.sendEvent.apply(page, event.args);
     }
 };
@@ -78,7 +81,7 @@ page.onCallback = function (event) {
     if (handler) {
         return handler(event);
     } else {
-        console.log(logHeader + " the page is trying to call an undefined callback: ",event.name);
+        console.log(logHeader + " the page is trying to call an undefined callback: ", event.name);
     }
 };
 
@@ -105,10 +108,14 @@ if (autoExit) {
         var topWindow = window.top;
         if (!topWindow.attester) {
             topWindow.console.log('autoexit check error: attester does not seem to be correctly loaded!');
-            topWindow.console.log("PHANTOMJS-ATTESTER-DISCONNECT");
+            topWindow.callPhantom({
+                name: 'attesterOnDisconnect'
+            });
         } else if (!topWindow.attester.connected) {
             topWindow.console.log('autoexit check error: attester does not seem to be connected!');
-            topWindow.console.log("PHANTOMJS-ATTESTER-DISCONNECT");
+            topWindow.callPhantom({
+                name: 'attesterOnDisconnect'
+            });
         }
     }).toString();
     setInterval(function () {