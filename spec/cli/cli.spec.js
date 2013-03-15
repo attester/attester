7dae08ea586314dfb2ae32a0bcfbecffe1e65052
fcrisci <fabio.crisci@amadeus.com>
2013-03-15 13:53:32 +0100

    Assert on test execution
    Add the possibility to assert attester test results, the number of test run, in error, with failures or skipped.

--- spec/cli/cli.spec.js
+++ spec/cli/cli.spec.js
@@ -26,6 +26,7 @@ describe('cli', function () {
             var exitCode = -1;
             var finished = false;
             var timedout = false;
+            var testExecution = null;
             runs(function () {
                 console.log('\n---------------------------------------');
                 console.log('Starting test: ' + options.testCase);
@@ -33,13 +34,32 @@ describe('cli', function () {
                 var args = [execPath].concat(options.args || []);
                 var spawnOpts = options.spawnOpts || {};
                 var timeout = null;
-                spawnOpts.stdio = 'inherit';
+                spawnOpts.stdio = ['ignore', 'pipe', 'pipe'];
                 var childProcess = child_process.spawn('node', args, spawnOpts);
                 childProcess.on('exit', function (code) {
                     if (!timedout) {
                         finished = true;
                         exitCode = code;
                         clearTimeout(timeout);
+                    }
+                });
+                childProcess.stdout.pipe(process.stdout, {
+                    end: false
+                });
+                childProcess.stderr.pipe(process.stderr, {
+                    end: false
+                });
+                childProcess.stdout.on('data', function (data) {
+                    // data is a buffer
+                    data = data.toString();
+                    var result = data.match(/tests run\s?\:\s?(\d+)\s?,\s?failures\s?\:\s?(\d+)\s?,\s?errors\s?\:\s?(\d+)\s?,\s?skipped\s?\:\s?(\d+)\s?/i);
+                    if (result) {
+                        testExecution = {
+                            run: parseInt(result[1], 10),
+                            failures: parseInt(result[2], 10),
+                            errors: parseInt(result[3], 10),
+                            skipped: parseInt(result[4], 10)
+                        };
                     }
                 });
                 timeout = setTimeout(function () {
@@ -52,6 +72,9 @@ describe('cli', function () {
             }, (options.timeout || defaultTimeout) + 100, 'attester to complete');
             runs(function () {
                 expect(exitCode).toEqual(options.exitCode);
+                if (options.results) {
+                    expect(testExecution).toEqual(options.results);
+                }
             });
         });
     };
@@ -59,31 +82,60 @@ describe('cli', function () {
     itRuns({
         testCase: 'succeeds',
         exitCode: 0,
-        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldSucceed', '--phantomjs-instances', '1', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js']
+        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldSucceed', '--phantomjs-instances', '1', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js'],
+        results: {
+            run: 1,
+            failures: 0,
+            errors: 0,
+            skipped: 0
+        }
     });
 
     itRuns({
         testCase: 'hasFailure',
         exitCode: 1,
-        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldFail', '--phantomjs-instances', '1', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js']
+        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldFail', '--phantomjs-instances', '1', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js'],
+        results: {
+            run: 1,
+            failures: 1,
+            errors: 0,
+            skipped: 0
+        }
     });
 
     itRuns({
         testCase: 'ignoreFailure',
         exitCode: 0,
-        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldFail', '--phantomjs-instances', '1', '--ignore-failures', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js']
+        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldFail', '--phantomjs-instances', '1', '--ignore-failures', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js'],
+        results: {
+            run: 1,
+            failures: 1,
+            errors: 0,
+            skipped: 0
+        }
     });
 
     itRuns({
         testCase: 'hasError',
         exitCode: 1,
-        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldRaiseError', '--phantomjs-instances', '1', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js']
+        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldRaiseError', '--phantomjs-instances', '1', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js'],
+        results: {
+            run: 1,
+            failures: 0,
+            errors: 1,
+            skipped: 0
+        }
     });
 
     itRuns({
         testCase: 'ignoreError',
         exitCode: 0,
-        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldRaiseError', '--phantomjs-instances', '1', '--ignore-errors', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js']
+        args: ['--config.resources./', atTestsRoot, '--config.resources./', atFrameworkPath, '--config.tests.aria-templates.classpaths.includes', 'test.attester.ShouldRaiseError', '--phantomjs-instances', '1', '--ignore-errors', '--config.coverage.files.rootDirectory', atTestsRoot, '--config.coverage.files.includes', '**/*.js'],
+        results: {
+            run: 1,
+            failures: 0,
+            errors: 1,
+            skipped: 0
+        }
     });
-
 });
