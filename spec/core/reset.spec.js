731f045024dbb4d0a00c0757d446d605635de358
fcrisci <fabio.crisci@amadeus.com>
2013-08-08 18:05:14 +0200

    Plugin for test pages
    After the refactoring this is the first pull request that allows using plugins, so far only to modify the content of a test page.
    
    This is achieved by exposing some middlewares and adding the modules
    
    - plugins where you can require a plugin from disk or npm
    - testPage where you can add element into a test page
    
    For testing purposes, this pull request introduces also an attester.dispose to cleanly dispose all objects created.
    
    Conflicts:
    	lib/attester/cli.js
    	lib/test-type/aria-templates/at-test-type.js
    	lib/test-type/base-test-type.js
    	lib/test-type/mocha/mocha-test-type.js

---
+++ spec/core/reset.spec.js
@@ -0,0 +1,60 @@
+/* globals describe, it, beforeEach, afterEach, expect */
+/*
+ * Copyright 2013 Amadeus s.a.s.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var path = require("path");
+
+var attester = require("../../lib/attester");
+
+describe("core", function () {
+    afterEach(function (done) {
+        attester.__reset__(done);
+    });
+
+    it("reset", function (done) {
+        // Start a campaign and then reset attester, both should work properly
+        startCampaign(function () {
+            attester.__reset__(function () {
+                startCampaign(function () {
+                    done();
+                });
+            });
+        });
+    });
+});
+
+function startCampaign(callback) {
+    attester.config.set({
+        "phantomjs-instances": 1
+        // enable debug logging
+        //, "log-level": 4
+    });
+
+    attester.campaign.create(attester.config.readFile(path.join(__dirname, "campaign/simpleTest.yaml")));
+
+    attester.event.once("attester.core.idle", function () {
+        process.nextTick(callback);
+    });
+    attester.event.once("attester.core.fail", function () {
+        throw "Didn't expect to fail";
+    });
+    attester.event.once("reports.stats", function (stats) {
+        expect(stats.testCases).toBe(2);
+        expect(stats.errors).toBe(0);
+        expect(stats.failures).toBe(0);
+    });
+
+    attester.start();
+}
