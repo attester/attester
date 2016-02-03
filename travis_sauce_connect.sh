546bb8079cc504850487cb76f06a677e18ed31c1
ddivernois <david-emmanuel.divernois@amadeus.com>
2016-02-03 15:03:48 +0100

    Removing the sauce_connect travis-ci addon

---
+++ travis_sauce_connect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir -p /tmp/sauce_connect
+cd /tmp/sauce_connect
+
+echo "Downloading Sauce Connect"
+
+SC_NAME=sc-4.3.13-linux
+
+wget "https://saucelabs.com/downloads/${SC_NAME}.tar.gz"
+
+echo "Extracting Sauce Connect"
+tar -zxf "${SC_NAME}.tar.gz"
+
+SC_READYFILE=sauce-connect-ready-$RANDOM
+
+echo "Starting Sauce Connect"
+./${SC_NAME}/bin/sc \
+  -i ${TRAVIS_JOB_NUMBER} \
+  -f ${SC_READYFILE} \
+  -l $HOME/sauce-connect.log &
+
+echo "Waiting for Sauce Connect readyfile"
+while [ ! -f ${SC_READYFILE} ]; do
+  sleep .5
+done
