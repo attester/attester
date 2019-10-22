744d2492e8133d40c6b0bd07310dddc672be380b
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2019-10-22 10:35:40 +0200

    Using GitHub workflows and Node.js 12 instead of travis-ci and older Node.js versions

--- sauce_connect.sh
+++ sauce_connect.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ "$SAUCE_USERNAME" == "" ] ||
+   [ "$SAUCE_ACCESS_KEY" == "" ] ||
+   [ "$SAUCE_TUNNEL_ID" == "" ] ; then
+  echo "Missing SAUCE_USERNAME or SAUCE_ACCESS_KEY or SAUCE_TUNNEL_ID."
+  echo "Not starting sauce connect."
+  exit 0 ;
+fi
+
 mkdir -p /tmp/sauce_connect
 cd /tmp/sauce_connect
 
 echo "Downloading Sauce Connect"
 
-SC_NAME=sc-4.4.9-linux
+SC_NAME=sc-4.6.2-linux
 
 wget "https://saucelabs.com/downloads/${SC_NAME}.tar.gz"
 
@@ -16,7 +24,7 @@ SC_READYFILE=sauce-connect-ready-$RANDOM
 
 echo "Starting Sauce Connect"
 ./${SC_NAME}/bin/sc \
-  -i ${TRAVIS_JOB_NUMBER} \
+  -i ${SAUCE_TUNNEL_ID} \
   -f ${SC_READYFILE} \
   -l $HOME/sauce-connect.log &
 
